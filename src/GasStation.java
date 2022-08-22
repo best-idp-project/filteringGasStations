a243a5d3f1e7d19c6d0a09ae0c5b0f4bbdccc1d8
lucafavaro24 <ge26xuv@mytum.de>
2022-08-22 18:39:18 +0200

    Initial commit

---
+++ src/GasStation.java
@@ -0,0 +1,9 @@
+public class GasStation {
+    double lat = 0.0;
+    double lon = 0.0;
+
+    public GasStation(double latitude, double longitude) {
+        lat = latitude;
+        lon = longitude;
+    }
+}
