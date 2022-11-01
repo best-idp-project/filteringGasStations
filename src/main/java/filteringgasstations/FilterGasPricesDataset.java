f9aac185639779d5c79fbc15f8a2276ecfcb0fa8
lucafavaro24 <ge26xuv@mytum.de>
2022-11-01 16:25:18 +0100

    Computed averages for german stations, printed in allStationsAndAvgPricesDE.csv (id,date,gasPrice). 249 stations are excluded as no gas price was available

--- src/main/java/filteringgasstations/FilterGasPricesDataset.java
+++ src/main/java/filteringgasstations/FilterGasPricesDataset.java
@@ -1,22 +1,32 @@
 package filteringgasstations;
 
 import com.opencsv.CSVReader;
+import filteringgasstations.stations.GasStationPair;
+import filteringgasstations.stations.GermanStation;
+import filteringgasstations.utils.PriceDatePair;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FilterGasPricesDataset {
+    static List<GermanStation> germanStations = germanStations();
 
     public static void main(String[] args) {
-        filterDatasetWithGermanStations();
+
+        readPricesSomeDaysBeforeStart();
+        readAllDaysPrices();
+        writeStationsAndAvgPrices();
     }
 
-    public static List<String> germanStations() {
-        List<String> germanStations = new ArrayList<>();
+    public static List<GermanStation> germanStations() {
+        List<GermanStation> germanStations = new ArrayList<>();
         var file = ClassLoader.getSystemClassLoader().getResource("germanStations20Km.csv");
 
         try {
@@ -24,7 +34,8 @@ public class FilterGasPricesDataset {
             try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
                 String[] lineInArray;
                 while ((lineInArray = reader.readNext()) != null) {
-                    germanStations.add(lineInArray[0]);
+                    GermanStation temp = new GermanStation(lineInArray[0]);
+                    germanStations.add(temp);
                 }
             }
         } catch (Exception e) {
@@ -35,26 +46,47 @@ public class FilterGasPricesDataset {
         return germanStations;
     }
 
-    public static void filterDatasetWithGermanStations() {
-        List<String> germanStations = germanStations();
+    public static void readPricesSomeDaysBeforeStart() {
+        for (int day = 10; day < 15; day++) {
+            String fileName = "prices/germany/extra/2022-04-" + day + "-prices.csv";
+            var file = ClassLoader.getSystemClassLoader().getResource(fileName);
+            try {
+                try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
+                    String[] lineInArray;
+                    reader.readNext();  // skip header
+                    while ((lineInArray = reader.readNext()) != null) {
+                        for (GermanStation i : germanStations) {
+                            if (i.id.equals(lineInArray[1])) {
+                                i.setLastPrice(Double.parseDouble(lineInArray[2]));
+                                break;
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Prices of the 5 days before start OK");
+    }
 
+    public static void readAllDaysPrices() {
         for (int month = 4; month <= 10; month++) {
             for (int day = 1; day <= 31; day++) {
-                List<String> filteredDataset = new ArrayList<>();
                 String fileName = "prices/germany/" + month + "/2022-" + String.format("%02d", month) + "-" + String.format("%02d", day) + "-prices.csv";
                 var file = ClassLoader.getSystemClassLoader().getResource(fileName);
                 if (file == null) continue;
                 try {
                     try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
                         String[] lineInArray;
-                        // add header
-                        filteredDataset.add(String.join(",", reader.readNext()));
-                        // add entries only in case of a match
+                        reader.readNext();  // skip header
                         while ((lineInArray = reader.readNext()) != null) {
-                            if (germanStations.contains(lineInArray[1])) {
-                                // remove outliers (in the raw file there are 0 and neg values)
-                                if (Double.parseDouble(lineInArray[2]) > 1)
-                                    filteredDataset.add(String.join(",", lineInArray));
+                            for (GermanStation i : germanStations) {
+                                if (i.id.equals(lineInArray[1])) {
+                                    i.addTimeAndPrice(Time.valueOf(lineInArray[0].substring(11, 19)), Double.parseDouble(lineInArray[2]));
+                                    //System.out.println(i.timestamps.get(i.timestamps.size() - 1));
+                                    break;
+                                }
                             }
                         }
                     }
@@ -62,32 +94,83 @@ public class FilterGasPricesDataset {
                     e.printStackTrace();
                 }
 
-                writeValidEntriesToFile(filteredDataset, fileName);
+                //System.out.println(germanStations);
+                endDayProcedure(Date.valueOf("2022-" + String.format("%02d", month) + "-" + String.format("%02d", day)));
+
             }
         }
-        System.out.println("All prices dataset are now filtered");
+
+        // remove stations that contain only zero values during the whole period of 6 months
+        AtomicInteger zeroStations = new AtomicInteger();
+        List<GermanStation> copy = new ArrayList<>(germanStations);
+        copy.forEach(station -> {
+            if (station.avgPrices.get(station.avgPrices.size() - 1).avgPrice == 0.0) {
+                zeroStations.getAndIncrement();
+                germanStations.remove(station);
+            } else
+                System.out.println(station.id + " " + station.avgPrices);
+        });
+        //System.out.println("missing prices counter: " + zeroStations.get());
 
     }
 
-    public static void writeValidEntriesToFile(List<String> filteredDataset, String fileName) {
+    public static void endDayProcedure(Date date) {
+        germanStations.forEach(station -> {
+            if (station.prices.size() == 0) {
+                // to improve this, manual import of prices for some days before the start of the analysis
+                station.avgPrices.add(new PriceDatePair(station.lastPrice, date));
+                //System.out.println(station.id + station.avgPrices);
+                return;
+            }
+            computeAveragePriceOfDay(station, date);
+            // set last price for the following day
+            station.setLastPrice(station.prices.get(station.prices.size() - 1));
+            // clear the price and timestamps arrays
+            station.prices.clear();
+            station.timestamps.clear();
+            //System.out.println(station.id + station.avgPrices);
+        });
+    }
 
-        File filecsv = new File("output/germany/");
+    public static void computeAveragePriceOfDay(GermanStation station, Date date) {
+        Double totalPrice = 0.0;
+        Double lastPrice = station.lastPrice;
+        Time lastTime = Time.valueOf("06:00:00");
+        for (int i = 0; i < station.timestamps.size(); i++) {
+            if (station.timestamps.get(i).before(Time.valueOf("06:00:00")))
+                station.setLastPrice(station.prices.get(i));
+            else if (station.timestamps.get(i).before(Time.valueOf("22:00:00"))) {
+                totalPrice += lastPrice * (station.timestamps.get(i).getTime() - lastTime.getTime()) / 1000;
+                lastPrice = station.prices.get(i);
+                lastTime = station.timestamps.get(i);
+            }
+        }
+        totalPrice += lastPrice * (Time.valueOf("22:00:00").getTime() - lastTime.getTime()) / 1000;
+        Double avgPrice = totalPrice / ((22 - 6) * 60 * 60);
+        station.avgPrices.add(new PriceDatePair(avgPrice, date));
+    }
+
+    public static void writeStationsAndAvgPrices() {
+
+        File filecsv = new File("output");
         if (!filecsv.exists()) {
             var _bool = filecsv.mkdir();
         }
 
         try {
-            filecsv = new File("output/" + fileName);
+            filecsv = new File("output/allStationsAndAvgPricesDE.csv");
             FileWriter fwcsv = new FileWriter(filecsv);
+            fwcsv.append("id,date,avgPrice\n");
 
-            for (String entry : filteredDataset) {
-                fwcsv.append(entry).append("\n");
+            for (GermanStation station : germanStations) {
+                for (PriceDatePair entry : station.avgPrices)
+                    fwcsv.append(station.id + "," + entry.date + "," + entry.avgPrice + "\n");
             }
             fwcsv.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Output produced: filtered datasets");
     }
+
 }
