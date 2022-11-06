a038d0f271fb708a2cfc9562c586241a545e6a6b
Andreas Resch <andreas@resch.io>
2022-11-06 22:59:07 +0100

    use Utils methods

--- src/main/java/filteringgasstations/FilterGasPricesDataset.java
+++ src/main/java/filteringgasstations/FilterGasPricesDataset.java
@@ -1,110 +1,79 @@
 package filteringgasstations;
 
+
 import com.opencsv.CSVReader;
 import filteringgasstations.geolocation.CountryCode;
 import filteringgasstations.stations.ForeignPriceEntry;
 import filteringgasstations.stations.GermanStation;
 import filteringgasstations.utils.PriceDatePair;
+import filteringgasstations.utils.Utils;
 import org.apache.commons.lang3.time.DateUtils;
 
-import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.Date;
 import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
-
 public class FilterGasPricesDataset {
-    static List<GermanStation> germanStations = germanStations();
+    private static final int ONE_WEEK = 604800000;
+    static List<GermanStation> germanStations;
 
     public static void main(String[] args) {
+        germanStations = germanStations();
         // FOR GERMAN STATIONS
-        //readPricesSomeDaysBeforeStart();
-        //readAllDaysPrices();
-        //writeStationsAndAvgPricesDE();
+        readPricesSomeDaysBeforeStart();
+        readAllDaysPrices();
+        writeStationsAndAvgPricesDE();
 
         // FOR FOREIGN STATIONS
         writeStationsAndAvgPricesFOREIGN();
-
     }
 
-    public static List<GermanStation> germanStations() {
-        List<GermanStation> germanStations = new ArrayList<>();
+    public static CopyOnWriteArrayList<GermanStation> germanStations() {
         var file = ClassLoader.getSystemClassLoader().getResource("germanStations20Km.csv");
-
-        try {
-            assert file != null;
-            try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
-                String[] lineInArray;
-                while ((lineInArray = reader.readNext()) != null) {
-                    GermanStation temp = new GermanStation(lineInArray[0]);
-                    germanStations.add(temp);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        List<GermanStation> germanStations = Utils.readCSV(file).stream().map(line -> new GermanStation(line[0])).toList();
         System.out.println("German stations correctly imported");
-        return germanStations;
+        return new CopyOnWriteArrayList<>(germanStations);
     }
 
     public static void readPricesSomeDaysBeforeStart() {
         for (int day = 1; day < 15; day++) {
             String fileName = "prices/germany/extra/2022-04-" + String.format("%02d", day) + "-prices.csv";
             var file = ClassLoader.getSystemClassLoader().getResource(fileName);
-            try {
-                try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
-                    String[] lineInArray;
-                    reader.readNext();  // skip header
-                    while ((lineInArray = reader.readNext()) != null) {
-                        for (GermanStation i : germanStations) {
-                            if (i.id.equals(lineInArray[1])) {
-                                i.setLastPrice(Double.parseDouble(lineInArray[2]));
-                                break;
-                            }
-                        }
+            Utils.readCSV(file).forEach(line -> {
+                for (GermanStation i : germanStations) {
+                    if (i.id.equals(line[1])) {
+                        i.setLastPrice(Double.parseDouble(line[2]));
+                        break;
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            });
         }
-        System.out.println("Prices of the 5 days before start OK");
+        System.out.println("Prices of the 15 days before start OK");
     }
-
     public static void readAllDaysPrices() {
         for (int month = 4; month <= 10; month++) {
             for (int day = 1; day <= 31; day++) {
                 String fileName = "prices/germany/" + month + "/2022-" + String.format("%02d", month) + "-" + String.format("%02d", day) + "-prices.csv";
                 var file = ClassLoader.getSystemClassLoader().getResource(fileName);
-                if (file == null) continue;
-                try {
-                    try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
-                        String[] lineInArray;
-                        reader.readNext();  // skip header
-                        while ((lineInArray = reader.readNext()) != null) {
-                            for (GermanStation i : germanStations) {
-                                if (i.id.equals(lineInArray[1])) {
-                                    i.addTimeAndPrice(Time.valueOf(lineInArray[0].substring(11, 19)), Double.parseDouble(lineInArray[2]));
-                                    //System.out.println(i.timestamps.get(i.timestamps.size() - 1));
-                                    break;
-                                }
-                            }
+                List<String[]> lines = Utils.readCSV(file);
+                if (lines.isEmpty()) {
+                    continue;
+                }
+                lines.forEach(line -> {
+                    for (GermanStation station : germanStations) {
+                        if (station.id.equals(line[1])) {
+                            station.addTimeAndPrice(Time.valueOf(line[0].substring(11, 19)), Double.parseDouble(line[2]));
+                            //System.out.println(i.timestamps.get(i.timestamps.size() - 1));
+                            break;
                         }
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                //System.out.println(germanStations);
+                });
                 endDayProcedure(Date.valueOf("2022-" + String.format("%02d", month) + "-" + String.format("%02d", day)));
-
             }
         }
 
@@ -141,116 +110,75 @@ public class FilterGasPricesDataset {
     }
 
     public static void computeAveragePriceOfDay(GermanStation station, Date date) {
-        Double totalPrice = 0.0;
+        double totalPrice = 0.0;
         Double lastPrice = station.lastPrice;
         Time lastTime = Time.valueOf("06:00:00");
+        final Time startTime = Time.valueOf("06:00:00");
+        final Time endTime = Time.valueOf("22:00:00");
         for (int i = 0; i < station.timestamps.size(); i++) {
-            if (station.timestamps.get(i).before(Time.valueOf("06:00:00")))
+            if (station.timestamps.get(i).before(startTime))
                 station.setLastPrice(station.prices.get(i));
-            else if (station.timestamps.get(i).before(Time.valueOf("22:00:00"))) {
+            else if (station.timestamps.get(i).before(endTime)) {
                 totalPrice += lastPrice * (station.timestamps.get(i).getTime() - lastTime.getTime()) / 1000;
                 lastPrice = station.prices.get(i);
                 lastTime = station.timestamps.get(i);
             }
         }
-        totalPrice += lastPrice * (Time.valueOf("22:00:00").getTime() - lastTime.getTime()) / 1000;
+        totalPrice += lastPrice * (endTime.getTime() - lastTime.getTime()) / 1000;
         Double avgPrice = totalPrice / ((22 - 6) * 60 * 60);
         station.avgPrices.add(new PriceDatePair(avgPrice, date));
     }
 
     public static void writeStationsAndAvgPricesDE() {
-
-        File filecsv = new File("output");
-        if (!filecsv.exists()) {
-            var _bool = filecsv.mkdir();
-        }
-
-        try {
-            filecsv = new File("output/allStationsAndAvgPricesDE.csv");
-            FileWriter fwcsv = new FileWriter(filecsv);
-            fwcsv.append("id,date,avgPrice\n");
-
-            for (GermanStation station : germanStations) {
-                for (PriceDatePair entry : station.avgPrices)
-                    fwcsv.append(station.id + "," + entry.date + "," + entry.avgPrice + "\n");
-            }
-            fwcsv.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        String filename = "output/allStationsAndAvgPricesDE.csv";
+        String[] columns = new String[]{
+                "id",
+                "date",
+                "avgPrice"
+        };
+        List<String> lines = new ArrayList<>();
+        germanStations.forEach(germanStation ->
+                germanStation.avgPrices.stream().filter(priceDatePair -> priceDatePair.date.after(Date.valueOf("2022-04-14"))).forEach(priceDatePair ->
+                        lines.add(germanStation.id + "," + priceDatePair.date + "," + priceDatePair.avgPrice)));
+        Utils.writeCSV(filename, columns, lines);
         System.out.println("All stations and avg prices for DE done");
 
     }
 
     public static List<ForeignPriceEntry> readForeignPriceDataset() {
-        List<ForeignPriceEntry> foreignPriceDataset = new ArrayList<>();
         var file = ClassLoader.getSystemClassLoader().getResource("prices/foreign/foreign_petrol_prices.csv");
-
-        try {
-            assert file != null;
-            try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
-                String[] lineInArray;
-                reader.readNext(); // skip header
-                while ((lineInArray = reader.readNext()) != null) {
-                    ForeignPriceEntry entry = new ForeignPriceEntry(CountryCode.valueOf(lineInArray[0]),
-                            Date.valueOf(lineInArray[1]), Double.parseDouble(lineInArray[2]));
-                    foreignPriceDataset.add(entry);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        List<ForeignPriceEntry> foreignPriceDataset = Utils.readCSV(file).stream().map(line -> new ForeignPriceEntry(CountryCode.valueOf(line[0]),
+                Date.valueOf(line[1]), Double.parseDouble(line[2]))).toList();
         System.out.println("Foreign price dataset OK");
         return foreignPriceDataset;
     }
 
     public static void writeStationsAndAvgPricesFOREIGN() {
         List<ForeignPriceEntry> foreignPriceDataset = readForeignPriceDataset();
-        File filecsv = new File("output");
-        if (!filecsv.exists()) {
-            var _bool = filecsv.mkdir();
-        }
-
-        try {
-            filecsv = new File("output/allStationsAndAvgPricesFOREIGN.csv");
-            FileWriter fwcsv = new FileWriter(filecsv);
-            fwcsv.append("id,date,avgPrice\n");
-
-            // for every station in the foreignStations20Km.csv
-            var file = ClassLoader.getSystemClassLoader().getResource("foreignStations20Km.csv");
-
-            try {
-                assert file != null;
-                try (CSVReader reader = new CSVReader(new FileReader(file.getPath()))) {
-                    String[] lineInArray;
-                    reader.readNext(); // skip header
-                    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                    while ((lineInArray = reader.readNext()) != null) {
-                        for (ForeignPriceEntry entry : foreignPriceDataset) {
-                            // find the first entry in foreign_petrol_prices.csv with same country code
-                            if (entry.countryCode == CountryCode.valueOf(lineInArray[1]))
-                                // 604 800 000 is one week in ms
-                                for (java.util.Date date = entry.date; date.before(new Date(entry.date.getTime() + 604800000)); date = DateUtils.addDays(date, 1)) {
-                                    if (date.before(Date.valueOf("2022-04-15")) || date.after(Date.valueOf("2022-10-15")))
-                                        continue;
-
-                                    fwcsv.append(lineInArray[0] + "," + dateFormat.format(date) + "," + entry.price + "\n");
-                                }
-                        }
+        String filename = "output/allStationsAndAvgPricesFOREIGN.csv";
+        String[] columns = new String[]{
+                "id",
+                "date",
+                "avgPrice"
+        };
+        List<String> lines = new ArrayList<>();
+        // for every station in the foreignStations20Km.csv
+        var file = ClassLoader.getSystemClassLoader().getResource("foreignStations20Km.csv");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        final Date start = Date.valueOf("2022-04-15");
+        final Date end = Date.valueOf("2022-10-15");
+        Utils.readCSV(file).forEach(line -> {
+            for (ForeignPriceEntry entry : foreignPriceDataset) {
+                // find the first entry in foreign_petrol_prices.csv with same country code
+                if (entry.countryCode == CountryCode.valueOf(line[1]))
+                    // 604 800 000 is one week in ms
+                    for (java.util.Date date = entry.date; date.before(new Date(entry.date.getTime() + ONE_WEEK)); date = DateUtils.addDays(date, 1)) {
+                        if (date.before(start) || date.after(end))
+                            continue;
+                        lines.add(line[0] + "," + dateFormat.format(date) + "," + entry.price);
                     }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-
-            fwcsv.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+        });
+        Utils.writeCSV(filename, columns, lines);
     }
-
 }
